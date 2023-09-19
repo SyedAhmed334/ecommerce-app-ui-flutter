e66d571950f945b58fe97d4a24f78df328bd3589
Syed Ahmed Shah <ali.sain334@gmail.com>
2023-09-20 00:09:25 +0500

    added changes in home page

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -9,43 +9,121 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                Image(
-                  image: AssetImage('assets/images/cover.jpg'),
-                  height: MediaQuery.of(context).size.height / 3.6,
-                  fit: BoxFit.contain,
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Image(
+                image: AssetImage('assets/images/cover.jpg'),
+                height: MediaQuery.of(context).size.height / 3.6,
+                fit: BoxFit.contain,
+              ),
+              Positioned(
+                top: 160,
+                left: 5,
+                child: Text(
+                  'Azeer\'s Products',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                 ),
-                Positioned(
-                  top: 160,
-                  left: 5,
-                  child: Text(
-                    'Azeer\'s Products',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ],
-            ),
-            Text(
-              'Products',
+              ),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 15\),
+            width: double.infinity,
+            child: Text(
+              'Earbuds',
               style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
             ),
-            GridView(
+          ),
+          Expanded(
+            child: GridView(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 0),
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 5),
               children: [
                 Card(
+                  color: Colors.grey[100],
                   child: Column(
-                    children: [],
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image(
+                        image: AssetImage('assets/images/earbuds1.jpg'),
+                        height: 100,
+                      ),
+                      Text('Product 1'),
+                    ],
+                  ),
+                ),
+                Card(
+                  color: Colors.grey[100],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image(
+                        image: AssetImage('assets/images/earbuds2.jpg'),
+                        height: 100,
+                      ),
+                      Text('Product 2'),
+                    ],
+                  ),
+                ),
+                Card(
+                  color: Colors.grey[100],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image(
+                        image: AssetImage('assets/images/earbuds3.jpg'),
+                        height: 100,
+                      ),
+                      Text('Product 3'),
+                    ],
+                  ),
+                ),
+                Card(
+                  color: Colors.grey[100],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image(
+                        image: AssetImage('assets/images/earbuds4.jpg'),
+                        height: 100,
+                      ),
+                      Text('Product 4'),
+                    ],
+                  ),
+                ),
+                Card(
+                  color: Colors.grey[100],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image(
+                        image: AssetImage('assets/images/earbuds5.jpg'),
+                        height: 100,
+                      ),
+                      Text('Product 5'),
+                    ],
+                  ),
+                ),
+                Card(
+                  color: Colors.grey[100],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image(
+                        image: AssetImage('assets/images/earbuds7.png'),
+                        height: 100,
+                      ),
+                      Text('Product 6'),
+                    ],
                   ),
                 ),
               ],
-            )
-          ],
-        ),
+            ),
+          ),
+        ],
       ),
     );
   }
