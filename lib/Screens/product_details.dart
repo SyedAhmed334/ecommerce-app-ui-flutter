854c24783e4817cfe8c1b35c8358d01d3e8dc22a
Syed Ahmed Shah <ali.sain334@gmail.com>
2023-09-22 01:44:54 +0500

    made some changes in product details screen

--- lib/Screens/product_details.dart
+++ lib/Screens/product_details.dart
@@ -23,73 +23,78 @@ class ProductDetails extends StatelessWidget {
                 width: double.infinity,
               ),
             ),
-            Container(
-              height: 390,
-              decoration: BoxDecoration(color: Colors.white),
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-                child: Column(
-                  children: [
-                    Text(
-                      title,
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: RichText(
-                        text: TextSpan(
-                          text: 'Rs. ',
-                          style: TextStyle(
-                              color: Theme.of(context).primaryColor,
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold),
-                          children: <TextSpan>[
-                            TextSpan(
-                                text: price,
-                                style: TextStyle(
-                                    fontSize:
-                                        30)), // Keep the remaining text as is
-                          ],
-                        ),
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Row(
+            Expanded(
+              child: Container(
+                // height: MediaQuery.of(context).size.height,
+                decoration: BoxDecoration(color: Colors.white),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
                         children: [
-                          TextButton.icon(
-                            style: ButtonStyle(
-                                foregroundColor:
-                                    MaterialStatePropertyAll(Colors.black54),
-                                padding: MaterialStatePropertyAll(
-                                  EdgeInsets.zero,
-                                )),
-                            onPressed: () {},
-                            icon: Icon(Icons.favorite_border),
-                            label: Text('Add to Wishlist'),
+                          Text(
+                            title,
+                            style: TextStyle(fontSize: 16),
                           ),
                           SizedBox(
-                            width: 50,
+                            height: 10,
                           ),
-                          TextButton.icon(
-                              style: ButtonStyle(
-                                  foregroundColor:
-                                      MaterialStatePropertyAll(Colors.black54),
-                                  padding: MaterialStatePropertyAll(
-                                      EdgeInsets.zero)),
-                              onPressed: () {},
-                              icon: Icon(Icons.share),
-                              label: Text('Share Product')),
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: RichText(
+                              text: TextSpan(
+                                text: 'Rs. ',
+                                style: TextStyle(
+                                    color: Theme.of(context).primaryColor,
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold),
+                                children: <TextSpan>[
+                                  TextSpan(
+                                      text: price,
+                                      style: TextStyle(
+                                          fontSize:
+                                              30)), // Keep the remaining text as is
+                                ],
+                              ),
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Row(
+                              children: [
+                                TextButton.icon(
+                                  style: ButtonStyle(
+                                      foregroundColor: MaterialStatePropertyAll(
+                                          Colors.black54),
+                                      padding: MaterialStatePropertyAll(
+                                        EdgeInsets.zero,
+                                      )),
+                                  onPressed: () {},
+                                  icon: Icon(Icons.favorite_border),
+                                  label: Text('Add to Wishlist'),
+                                ),
+                                SizedBox(
+                                  width: 50,
+                                ),
+                                TextButton.icon(
+                                    style: ButtonStyle(
+                                        foregroundColor:
+                                            MaterialStatePropertyAll(
+                                                Colors.black54),
+                                        padding: MaterialStatePropertyAll(
+                                            EdgeInsets.zero)),
+                                    onPressed: () {},
+                                    icon: Icon(Icons.share),
+                                    label: Text('Share Product')),
+                              ],
+                            ),
+                          ),
                         ],
                       ),
-                    ),
-                    Container(
-                      height: 240,
-                      child: Align(
+                      Align(
                         alignment: Alignment.bottomRight,
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.end,
@@ -119,8 +124,8 @@ class ProductDetails extends StatelessWidget {
                           ],
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
