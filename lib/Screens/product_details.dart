7ec64cf8a92ee9d9d22f21307a3a08d31b14554b
Syed Ahmed Shah <ali.sain334@gmail.com>
2023-09-23 20:02:27 +0500

    added cart screen

--- lib/Screens/product_details.dart
+++ lib/Screens/product_details.dart
@@ -2,135 +2,142 @@
 
 import 'package:flutter/material.dart';
 
-class ProductDetails extends StatelessWidget {
+class ProductDetails extends StatefulWidget {
   final String url;
   final String title;
   final String price;
   ProductDetails({required this.url, required this.title, required this.price});
 
   @override
+  State<ProductDetails> createState() => _ProductDetailsState();
+}
+
+class _ProductDetailsState extends State<ProductDetails> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: Padding(
-        padding: const EdgeInsets.only(top: 40.0),
-        child: Column(
-          children: [
-            Card(
-              child: Image.asset(
-                url,
-                fit: BoxFit.cover,
-                width: double.infinity,
-              ),
+      appBar: AppBar(title: Text('Ecommerce App'), actions: [
+        Padding(
+          padding: const EdgeInsets.only(right: 8.0),
+          child: Icon(Icons.shopping_cart),
+        ),
+      ]),
+      body: Column(
+        children: [
+          Card(
+            child: Image.asset(
+              widget.url,
+              fit: BoxFit.cover,
+              width: double.infinity,
             ),
-            Expanded(
-              child: Container(
-                // height: MediaQuery.of(context).size.height,
-                decoration: BoxDecoration(color: Colors.white),
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        children: [
-                          Text(
-                            title,
-                            style: TextStyle(fontSize: 16),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Align(
-                            alignment: Alignment.centerLeft,
-                            child: RichText(
-                              text: TextSpan(
-                                text: 'Rs. ',
-                                style: TextStyle(
-                                    color: Theme.of(context).primaryColor,
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.bold),
-                                children: <TextSpan>[
-                                  TextSpan(
-                                      text: price,
-                                      style: TextStyle(
-                                          fontSize:
-                                              30)), // Keep the remaining text as is
-                                ],
-                              ),
+          ),
+          Expanded(
+            child: Container(
+              // height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(color: Colors.white),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          widget.title,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: RichText(
+                            text: TextSpan(
+                              text: 'Rs. ',
+                              style: TextStyle(
+                                  color: Theme.of(context).primaryColor,
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold),
+                              children: <TextSpan>[
+                                TextSpan(
+                                    text: widget.price,
+                                    style: TextStyle(
+                                        fontSize:
+                                            30)), // Keep the remaining text as is
+                              ],
                             ),
                           ),
-                          Align(
-                            alignment: Alignment.centerLeft,
-                            child: Row(
-                              children: [
-                                TextButton.icon(
+                        ),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Row(
+                            children: [
+                              TextButton.icon(
+                                style: ButtonStyle(
+                                    foregroundColor: MaterialStatePropertyAll(
+                                        Colors.black54),
+                                    padding: MaterialStatePropertyAll(
+                                      EdgeInsets.zero,
+                                    )),
+                                onPressed: () {},
+                                icon: Icon(Icons.favorite_border),
+                                label: Text('Add to Wishlist'),
+                              ),
+                              SizedBox(
+                                width: 50,
+                              ),
+                              TextButton.icon(
                                   style: ButtonStyle(
                                       foregroundColor: MaterialStatePropertyAll(
                                           Colors.black54),
                                       padding: MaterialStatePropertyAll(
-                                        EdgeInsets.zero,
-                                      )),
+                                          EdgeInsets.zero)),
                                   onPressed: () {},
-                                  icon: Icon(Icons.favorite_border),
-                                  label: Text('Add to Wishlist'),
-                                ),
-                                SizedBox(
-                                  width: 50,
-                                ),
-                                TextButton.icon(
-                                    style: ButtonStyle(
-                                        foregroundColor:
-                                            MaterialStatePropertyAll(
-                                                Colors.black54),
-                                        padding: MaterialStatePropertyAll(
-                                            EdgeInsets.zero)),
-                                    onPressed: () {},
-                                    icon: Icon(Icons.share),
-                                    label: Text('Share Product')),
-                              ],
-                            ),
+                                  icon: Icon(Icons.share),
+                                  label: Text('Share Product')),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          ElevatedButton(
+                            style: ButtonStyle(
+                                shape: MaterialStatePropertyAll(
+                                    RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(0))),
+                                elevation: MaterialStatePropertyAll(0)),
+                            onPressed: () {},
+                            child: Text('Buy Now'),
+                          ),
+                          ElevatedButton(
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStatePropertyAll(Colors.orange),
+                                shape: MaterialStatePropertyAll(
+                                    RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(0))),
+                                elevation: MaterialStatePropertyAll(0)),
+                            onPressed: () {},
+                            child: Text('Add to Cart'),
                           ),
                         ],
                       ),
-                      Align(
-                        alignment: Alignment.bottomRight,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            ElevatedButton(
-                              style: ButtonStyle(
-                                  shape: MaterialStatePropertyAll(
-                                      RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(0))),
-                                  elevation: MaterialStatePropertyAll(0)),
-                              onPressed: () {},
-                              child: Text('Buy Now'),
-                            ),
-                            ElevatedButton(
-                              style: ButtonStyle(
-                                  backgroundColor:
-                                      MaterialStatePropertyAll(Colors.orange),
-                                  shape: MaterialStatePropertyAll(
-                                      RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(0))),
-                                  elevation: MaterialStatePropertyAll(0)),
-                              onPressed: () {},
-                              child: Text('Add to Cart'),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
